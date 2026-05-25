9cd65e8eb560f968fafaa90ffe28bf2ecfb1b3ff
To-Rex <torex.amaki@gmail.com>
2026-05-26 01:13:12 +0500

    Remove sensitive file and add to gitignore

--- config/database.py
+++ config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from apps.base.models import Base
 
-DATABASE_URL = 'postgresql+asyncpg://torex@localhost:5432/mx_soft_db'
+DATABASE_URL = 'postgresql+asyncpg://torex@distr.mxsoft.uz:5432/mx_soft_db'
 
 #if not DATABASE_URL:
     #DATABASE_URL = "postgresql+asyncpg://postgres:toor@distr.mxsoft.uz:5432/mx_soft_db"
