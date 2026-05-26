bd307aa0af4f5825a772a6bd5549981eeae7c587
To-Rex <torex.amaki@gmail.com>
2026-05-26 12:06:26 +0500

    fix

--- config/database.py
+++ config/database.py
@@ -9,13 +9,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL","")
 
-# subprocess.run("apt update", shell=True, check=True)
-# subprocess.run("apt install -y postgresql-client-18", shell=True, check=True)
-cmd = """
-apt update && apt install -y postgresql-client-18
-"""
+subprocess.run("apt update", shell=True, check=True)
+subprocess.run("apt install -y postgresql-client-18", shell=True, check=True)
 
-subprocess.run(cmd, shell=True, check=True)
 
 #if not DATABASE_URL:
     #DATABASE_URL = "postgresql+asyncpg://postgres:toor@distr.mxsoft.uz:5432/mx_soft_db"
