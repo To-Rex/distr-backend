96682a582378e9e9359f151fd948c09fd53be28d
To-Rex <torex.amaki@gmail.com>
2026-05-28 19:23:35 +0500

    fix

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,6 +3,7 @@ from apps.base.models import Base
 from apps.user.models import User, AccessToken
 from apps.company.models import Company
 from apps.branch.models import Branch
+from apps.device.models import Device
 from apps.location.models import Location
 from apps.app_version.models import App, Version
 from apps.working_session_tracking.models import WorkingSession
@@ -10,7 +11,7 @@ from apps.notification.models import Notification, NotificationUserStatus
 
 __all__ = [
     "Base", "User", "AccessToken",
-    "Company", "Branch", "Location",
+    "Company", "Branch", "Device", "Location",
     "App", "Version", "WorkingSession",
     "Notification", "NotificationUserStatus"
 ]
